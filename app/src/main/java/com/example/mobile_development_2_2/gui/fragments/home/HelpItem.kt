7d6c64dd958dacdd14c3d89a6046fa804f8da031
Coen <cc.denboer1@student.avans.nl>
2022-12-21 13:59:18 +0100

    added info images

--- app/src/main/java/com/example/mobile_development_2_2/gui/fragments/home/HelpItem.kt
+++ app/src/main/java/com/example/mobile_development_2_2/gui/fragments/home/HelpItem.kt
@@ -12,14 +12,14 @@ class HelpItem(title: String, imgId: Int, description: String) {
 
     companion object{
         fun getItems(): List<HelpItem> {
-            var item1 = HelpItem(Lang.get(R.string.HelpItem1), R.drawable.ic_launcher_background, Lang.get(R.string.HelpItem1Description))
-            var item2 = HelpItem(Lang.get(R.string.HelpItem2), R.drawable.ic_launcher_background, Lang.get(R.string.HelpItem2Description))
-            var item3 = HelpItem(Lang.get(R.string.HelpItem3), R.drawable.ic_launcher_background, Lang.get(R.string.HelpItem3Description))
-            var item4 = HelpItem(Lang.get(R.string.HelpItem4), R.drawable.ic_launcher_background, Lang.get(R.string.HelpItem4Description))
-            var item5 = HelpItem(Lang.get(R.string.HelpItem5), R.drawable.ic_launcher_background, Lang.get(R.string.HelpItem5Description))
-            var item6 = HelpItem(Lang.get(R.string.HelpItem6), R.drawable.ic_launcher_background, Lang.get(R.string.HelpItem6Description))
-            var item7 = HelpItem(Lang.get(R.string.HelpItem7), R.drawable.ic_launcher_background, Lang.get(R.string.HelpItem7Description))
-            var item8 = HelpItem(Lang.get(R.string.HelpItem8), R.drawable.ic_launcher_background, Lang.get(R.string.HelpItem8Description))
+            var item1 = HelpItem(Lang.get(R.string.HelpItem1), R.drawable.img_inforoute, Lang.get(R.string.HelpItem1Description))
+            var item2 = HelpItem(Lang.get(R.string.HelpItem2), R.drawable.img_infomap, Lang.get(R.string.HelpItem2Description))
+            var item3 = HelpItem(Lang.get(R.string.HelpItem3), R.drawable.img_infopause, Lang.get(R.string.HelpItem3Description))
+            var item4 = HelpItem(Lang.get(R.string.HelpItem4), R.drawable.img_infopoi, Lang.get(R.string.HelpItem4Description))
+            var item5 = HelpItem(Lang.get(R.string.HelpItem5), R.drawable.img_infopoimap, Lang.get(R.string.HelpItem5Description))
+            var item6 = HelpItem(Lang.get(R.string.HelpItem6), R.drawable.img_infopoilist, Lang.get(R.string.HelpItem6Description))
+            var item7 = HelpItem(Lang.get(R.string.HelpItem7), R.drawable.img_infolang, Lang.get(R.string.HelpItem7Description))
+            var item8 = HelpItem(Lang.get(R.string.HelpItem8), R.drawable.img_infocolor, Lang.get(R.string.HelpItem8Description))
 
             return listOf(item1, item2, item3, item4, item5, item6, item7, item8)
         }
