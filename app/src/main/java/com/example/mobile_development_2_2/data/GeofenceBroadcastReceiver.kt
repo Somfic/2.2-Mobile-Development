f2007735f19fad48e2e879fa50c3468a879a4d83
Momin2004 <m.musa@student.avans.nl>
2022-12-22 11:28:15 +0100

    geo manager

--- app/src/main/java/com/example/mobile_development_2_2/data/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/example/mobile_development_2_2/data/GeofenceBroadcastReceiver.kt
@@ -1,6 +1,8 @@
 package com.example.mobile_development_2_2.data
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
+import android.content.ContentValues
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
@@ -11,13 +13,12 @@ import com.example.mobile_development_2_2.gui.MainActivity
 import com.example.mobile_development_2_2.gui.fragments.MapFragment
 import com.example.mobile_development_2_2.map.route.Route
 import com.example.mobile_development_2_2.map.route.RouteManager
-import com.google.android.gms.location.Geofence
-import com.google.android.gms.location.GeofenceStatusCodes
-import com.google.android.gms.location.GeofencingEvent
+import com.google.android.gms.location.*
 
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
     private val TAG = "GeofenceBroadcastReceiver"
+
     // ...
     override fun onReceive(context: Context, intent: Intent) {
         var notificationHelper = NotificationHelper(context)
@@ -45,6 +46,7 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
                 PopupHelper.SetState(true)
 
                 Route.getNextPOI()
+
             }
         }
     }
--- app/src/main/java/com/example/mobile_development_2_2/map/route/RouteManager.kt
+++ app/src/main/java/com/example/mobile_development_2_2/map/route/RouteManager.kt
@@ -1,21 +1,28 @@
 package com.example.mobile_development_2_2.map.route
 
+import android.app.PendingIntent
+import android.content.ContentValues
 import android.content.Context
 import android.content.res.Resources
 import android.util.Log
 import com.example.mobile_development_2_2.R
+import com.example.mobile_development_2_2.data.GeofenceHelper
 import com.example.mobile_development_2_2.data.Lang
 import com.example.mobile_development_2_2.gui.fragments.home.HelpItem
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.google.gson.Gson
 
-class RouteManager {
+class RouteManager(context: Context) {
 
 
-    companion object{
+    companion object {
         var targetPOI: POI? = null
 
         var routes = TestRoutes()
-         private fun TestRoutes(): List<Route>{
+        private fun TestRoutes(): List<Route> {
             var testRoute1 = Route.TestRoute("testRoute1")
             var testRoute2 = Route.TestRoute2("testRoute2")
             //var testRoute3 = Route.TestRoute("testRoute3")
@@ -28,38 +35,42 @@ class RouteManager {
 
             return routes
         }
-        fun GetRoutes(resources: Resources): List<Route>{
-            val jsonString: String = resources.openRawResource(R.raw.historische_kilometer).bufferedReader().use { it.readText() }
+
+        fun GetRoutes(resources: Resources): List<Route> {
+            val jsonString: String =
+                resources.openRawResource(R.raw.historische_kilometer).bufferedReader()
+                    .use { it.readText() }
             val gson = Gson()
-            val routes =  gson.fromJson(jsonString, Array<Route>::class.java).toList()
+            val routes = gson.fromJson(jsonString, Array<Route>::class.java).toList()
             for (it in routes) {
-                for(poi in it.POIs){
+                for (poi in it.POIs) {
                     poi.visited = false
-                    if (poi.imgMap == null){
+                    if (poi.imgMap == null) {
                         poi.imgMap = "image404.png"
                     }
-                    if (poi.img == null){
+                    if (poi.img == null) {
                         poi.img = "image404.png"
                     }
                 }
             }
             return routes
         }
+
         var selectedItem = TestRoutes().get(0)
 
-        fun setRouteState(started: Boolean){
+        fun setRouteState(started: Boolean) {
             getRouteByName(selectedItem.name)?.started?.value = started
         }
 
-        fun getRouteByName(name : String) : Route?{
-            for (route in routes){
-                if(route.name == name)
+        fun getRouteByName(name: String): Route? {
+            for (route in routes) {
+                if (route.name == name)
                     return route
             }
             return null
         }
 
-        fun selectItem(route: Route){
+        fun selectItem(route: Route) {
             Log.d("a", "Item selected")
             selectedItem = route
         }
@@ -69,22 +80,24 @@ class RouteManager {
             return selectedItem
         }
 
-        fun selectPOI(poi:POI){
+        fun selectPOI(poi: POI) {
             Route.selectItem(poi)
         }
 
-        fun getSelectedPOI(): POI{
+        fun getSelectedPOI(): POI {
             return Route.getSelectedPOI()
         }
 
-        fun getNextPOI() : POI? {
+        fun getNextPOI(): POI? {
             return Route.getNextPOI()
         }
 
-        fun getStringById(context: Context, idName: String): String{
+        fun getStringById(context: Context, idName: String): String {
             val resources = context.resources
 
             return Lang.get(resources.getIdentifier(idName, "string", context.packageName))
         }
-    }
+
+}
+
 }
